30523864f68cf6980d49cc3672fc99becb6017fe
Filip <filip.pintea@gmail.com>
2026-03-06 14:10:13 +0200

    initial commit

---
+++ backend/src/main/java/com/smartcity/fixmystreet/FixmystreetApplication.java
@@ -0,0 +1,13 @@
+package com.smartcity.fixmystreet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixmystreetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixmystreetApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/smartcity/fixmystreet/FixmystreetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcity.fixmystreet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixmystreetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
